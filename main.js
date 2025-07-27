5e367707ea130caa5ee2ebe0f66d2ecb11ca34bd
Weiwei <chinweihsu83221@gmail.com>
2025-07-28 01:45:10 +0800

    首頁完成

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ import './assets/scss/allStytle.css';
 import './assets/scss/detailsStytle.css';
 import './assets/scss/listStytle.css';
 import './assets/scss/aboutStytle.css';
+import './assets/scss/indexStytle.css';
