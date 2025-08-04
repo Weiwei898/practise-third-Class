47e0e254283f93848fc8618901c38cafa611277f
Weiwei <chinweihsu83221@gmail.com>
2025-08-05 00:39:10 +0800

    LV3 完成測試

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ import './assets/scss/detailsStytle.css';
 import './assets/scss/listStytle.css';
 import './assets/scss/aboutStytle.css';
 import './assets/scss/indexStytle.css';
+import './assets/scss/collectStytle.css';
+import './assets/scss/logoin.css';
